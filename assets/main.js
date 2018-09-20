08c858da649b6e0fd19226fd1855a3c53bad0b06
justnsig11 <js365w@att.com>
2018-09-19 22:31:18 -0700

    final touches

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-// Initialize Firebase
+// firebase info
 var config = {
     apiKey: "AIzaSyDpm9OITGdxjMPX0ra0ZNIrMA4Sl2sLhSg",
     authDomain: "my-project-1535422874483.firebaseapp.com",
@@ -11,15 +11,19 @@ firebase.initializeApp(config);
 
 database = firebase.database();
 
+//document ready run javaScript
 $(document).ready(function () {
     console.log("ready");
+
+    //clock function
     clock();
 
+    //Adding clock to the header
     function clock() {
         setTimeout(clock, 1000);
         $("#currentTime").html(moment().format("LTS"));
     }
-
+    //Taking the information from the form 
     $("button").click(function (event) {
         event.preventDefault();
         var name = $("#trainName")
@@ -34,6 +38,8 @@ $(document).ready(function () {
         var frequency = $("#frequency")
             .val()
             .trim();
+
+        //copied and pasted this not 100% how it functions yet...seems to be working though
         firstTrainTime = moment(firstTrainTime, "LTS").format("LTS");
         firstTrainTimeMoment = moment(firstTrainTime, "LTS").subtract(1, "years");
         currentTime = moment();
@@ -44,6 +50,7 @@ $(document).ready(function () {
         nextTrainMoment = moment(nextTrain).format("LTS");
         console.log(nextTrainMoment);
 
+        //Pushing the info from the input fields and sending it to fireBase
         database.ref().push({
             name: name,
             destination: destination,
@@ -52,27 +59,28 @@ $(document).ready(function () {
             nextTrainMoment: nextTrainMoment,
             dateAdded: moment().format("LTS")
         });
-
+        //Setting variables as an empty string
         name = "";
         destination = "";
         firstTrainTime = "";
         frequency = "";
-
+        
         $("#trainName").val(name);
         $("#destination").val(destination);
         $("#firstTrain").val(firstTrainTime);
         $("#frequency").val(frequency);
 
     });
-
+    //Adding the informtion to firebase as a child.
     database.ref().on("child_added", function (snapshot) {
         trainName = snapshot.val().name;
         trainDest = snapshot.val().destination;
         firstTrain = snapshot.val().firstTrainTime;
         freq = snapshot.val().nextTrainMoment;
-
+        
+        //Adding the information to the DOM as a table
         $("#trains").append(
-            '<tr><td><i class="fas fa-subway"></i></td><td id="name">' +
+            '<tr><td><i></i></td><td id="name">' +
             trainName +
             '</td><td id="dest">' +
             trainDest +
